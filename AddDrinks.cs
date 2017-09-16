7642d897d0e9b72f3908ac2b4031d641cb3951fa
uRGGI <31953617+uRGGI@users.noreply.github.com>
2017-09-16 16:45:20 +0200

    Add files via upload

---
+++ AddDrinks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffeautomat
+{
+    public static class AddDrinks
+    {
+        
+        public static CocaCola AddCola()
+        {
+            return new CocaCola(10);
+        }
+
+        public static Fanta AddFanta()
+        {
+            return new Fanta(10);
+        }
+
+        public static Sprite AddSprite()
+        {
+            return new Sprite(10);
+        }
+
+        public static Juice AddJuice()
+        {
+            return new Juice(15);
+        }
+
+        public static Vatten AddWater()
+        {
+            return new Vatten(5);
+        }
+
+        public static Kaffe AddCoffe()
+        {
+            return new Kaffe(8);
+        }
+    }
+}
